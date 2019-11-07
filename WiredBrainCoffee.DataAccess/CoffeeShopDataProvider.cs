5c5d460feacaa0010ce6770eab74817bcb989bc5
Ahmed Maamoun <ahmedfcisasu@gmail.com>
2019-11-07 14:15:49 +0200

    Add PaperCup property

--- WiredBrainCoffee.DataAccess/CoffeeShopDataProvider.cs
+++ WiredBrainCoffee.DataAccess/CoffeeShopDataProvider.cs
@@ -4,10 +4,26 @@ namespace WiredBrainCoffee.DataAccess
 {
     public class CoffeeShopDataProvider
     {
-        public IEnumerable<CoffeeShop> LoadCoffeeShops() {
-            yield return new CoffeeShop { Location = "Nasr City", BeansInStokInKg = 105 };
-            yield return new CoffeeShop { Location = "Nasr City2", BeansInStokInKg = 106 };
-            yield return new CoffeeShop { Location = "Nasr City3", BeansInStokInKg = 107 };
+        public IEnumerable<CoffeeShop> LoadCoffeeShops()
+        {
+            yield return new CoffeeShop
+            {
+                Location = "Nasr City",
+                BeansInStokInKg = 105,
+                PaperCupsInStock = 150
+            };
+            yield return new CoffeeShop
+            {
+                Location = "Nasr City2",
+                BeansInStokInKg = 106,
+                PaperCupsInStock = 155
+            };
+            yield return new CoffeeShop
+            {
+                Location = "Nasr City3",
+                BeansInStokInKg = 107,
+                PaperCupsInStock = 160
+            };
 
         }
     }
