b2762ea6da2e455d172e883b12c87bab13cadf36
Ahmed Maamoun <ahmed.maamoun@tpay.me>
2019-11-07 13:50:36 +0200

    add quit command

--- WiredBrainCoffee ShopInfo Tool/Program.cs
+++ WiredBrainCoffee ShopInfo Tool/Program.cs
@@ -8,12 +8,17 @@ namespace WiredBrainCoffee_ShopInfo_Tool
         static void Main(string[] args)
         {
             Console.WriteLine("WiredBrainCoffee ShopInfo Tool!");
-            Console.WriteLine("Write 'help' to list available commands");
+            Console.WriteLine("Write 'help' to list available commands,"+"write 'quit' to exit applicationa");
             var coffeShopDataProvider = new CoffeeShopDataProvider();
             while (true)
             {
                 var line = Console.ReadLine();
                 var coffeShops = coffeShopDataProvider.LoadCoffeeShops();
+
+                if (string.Equals("quit", line, StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
                 if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
                 {
                     Console.WriteLine("> Available coffee shop commands:");
